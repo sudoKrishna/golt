9638b94d1f6ab82a69b0744e34e56b1141cce395
Krishna <krishnachaudhary2007gt@gmail.com>
2026-07-18 23:24:11 +0530

    feat(generate): enhance code generation endpoint with streaming response and file handling

--- apps/backend/src/routes/generate.ts
+++ apps/backend/src/routes/generate.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import { requireAuth } from "../middlewares/auth.middleware";
 import { prisma } from "@repo/db";
+import { generateCode, parseFiles } from "../lib/gemini";
 
 const router = Router();
-
+// sse is the rule that send data to the browser
+// res.write is because res.json and res.send closed the connection 
+// but there connection have to open so we use write
+function sendSSE(res : any , data : object) {
+    res.write(`data : ${JSON.stringify(data)}\n\n`)
+}
 
 router.post("/generate/:projectId",requireAuth,  async (req , res , next) => {
      const ownerId = (req as any).ownerId as string;
      const  projectId  = req.params.projectId as string;
      const {prompt} = req.body;
 
-     const project = prisma.project.findFirst({
+     const project = await prisma.project.findFirst({
         where : {
             id : projectId,
             ownerId : ownerId
@@ -22,7 +28,7 @@ router.post("/generate/:projectId",requireAuth,  async (req , res , next) => {
             error : "Project was not found"
         })
      }
-     const existingFiles = prisma.projectFile.findMany({
+     const existingFiles = await prisma.projectFile.findMany({
         where : {projectId},
         select : {path : true , content : true}
      })
@@ -30,6 +36,52 @@ router.post("/generate/:projectId",requireAuth,  async (req , res , next) => {
      res.setHeader("Content-Type" , "text/event-stream");
      res.setHeader("Cache-Control" , "no-cache");
      res.setHeader("Connection", "keep-alive");
+     res.flushHeaders();
 
+     let fullResponse = "";
+
+     const stream = await generateCode(prompt , existingFiles);
+
+     for await (const chunk of stream) {
+        const chunkText = chunk.text;
+
+        if(chunkText) {
+            fullResponse += chunkText;
+        }
+
+        sendSSE(res, {
+            type : "chunk",
+            content : chunkText,
+        })
+     }
+
+     const files = parseFiles(fullResponse);
+     
+     await Promise.all(
+        files.map((file) => 
+            prisma.projectFile.upsert({
+                where : {
+                    projectId_path : {
+                        projectId,
+                        path : file.path,
+                    },
+                },
+                update : {
+                    content : file.content,
+                },
+                create : {
+                    projectId ,
+                    path : file.path,
+                    content : file.content
+                }
+            })
+        )
+     )
+     sendSSE(res ,  {
+        type : "done",
+        files,
+     })
+
+     res.end();
 
 })
