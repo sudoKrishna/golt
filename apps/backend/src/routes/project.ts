ebeea73a4be5e1bbca80a2e14b424076c9691873
Krishna <krishnachaudhary2007gt@gmail.com>
2026-07-18 09:25:16 +0530

    feat(project): implement CRUD operations for projects

---
+++ apps/backend/src/routes/project.ts
@@ -0,0 +1,100 @@
+import { Router } from "express";
+import {prisma} from "@repo/db"
+
+const router = Router()
+
+// POST /projects — naya project banao DB mein
+// GET /projects — logged in user ke saare projects
+// GET /projects/:id — single project with files
+// DELETE /projects/:id — project delete
+
+router.post("/projects" , async (req , res, next) => {
+    try {
+        const ownerId = (req  as any).ownerId as string;
+        const {name , sandboxId} = req.body;
+
+         if(!name) {
+            return res.status(400).json({
+                error : "invalid project name"
+            })
+         }
+
+         const project = await prisma.project.create({
+            data : {
+                name, 
+                ownerId,
+                sandboxId
+            }
+         })
+
+         return res.status(201).json({project})
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/projects" , async (req , res , next) => {
+    const ownerId = (req as any) as string
+    const {project} = req.body;
+
+    const projects = await prisma.project.findMany({
+        where : {ownerId},
+        orderBy : {createAt : "desc"}
+    })
+
+    return res.status(200).json({projects})
+})
+
+router.get("projects/:id" , async (req , res, next) => {
+   try {
+     const ownerId  = (req as any).orderId as string
+     const {id} = req.params;
+ 
+     const project = await prisma.project.findFirst({
+         where : {id ,ownerId},
+         include : {
+             files : true,
+             message : {
+                 orderBy  : {createdAt : "asc"}
+             }
+         }
+     })
+ 
+     if(!project) {
+         return res.status(400).json({
+             error : "project was not found"
+         })
+     }
+ 
+     return res.status(200).json({
+         project
+     })
+   } catch (error) {
+    next(error)
+   }
+})
+
+router.delete("projects/:id" , async (req , res, next) => {
+   try {
+     const ownerId = (req as any).ownerId as string
+     const {id} = req.params;
+ 
+     const project = prisma.project.findFirst({
+         where : {id, ownerId},
+ 
+     })
+     if(!project) {
+         return res.status(400).json({
+             error : "project not found"
+         })
+     }
+ 
+     await prisma.project.delete({
+         where : {id}
+     })
+ 
+     return res.status(200).json({message : "Project deleted"})
+   } catch (error) {
+    next(error)
+   }
+})
